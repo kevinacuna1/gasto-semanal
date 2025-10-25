4cd86fce565d21a60d1db93d60d3ad81641bf0e1
Kevin Acuña <94419050+kevinacuna1@users.noreply.github.com>
2025-10-25 17:09:18 -0300

    Creando e instanciando la clase de Presupuesto

--- js/app.js
+++ js/app.js
@@ -6,21 +6,36 @@ const gastoListado = document.querySelector('#gastos ul');
 eventListeners();
 function eventListeners() {
     document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
-    formulario.addEventListener('submit', agregarGasto);
 }
 
 // Clases
+class Presupuesto {
+    constructor(presupuesto) {
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    }
+}
+
+class UI {
+
+}
+
+// Instanciar
+const ui = new UI();
+let presupuesto;
 
 // Funciones
 function preguntarPresupuesto() {
     const presupuestoUsuario = prompt('¿Cuál es tu presupuesto semanal?');
 
     // Validar el presupuestoUsuario
-    if(presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) {
+    if (presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) {
         window.location.reload();
     }
 
     // Si todo es correcto, asignar a la variable presupuesto
-    presupuesto = parseFloat(presupuestoUsuario);
+    presupuesto = new Presupuesto(presupuestoUsuario);
+
     console.log(presupuesto);
 }
